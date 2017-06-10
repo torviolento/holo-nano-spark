af6786a8b8dd4a542867c3497cb4fb31a2b0030f
Joel Hokkanen <joel.hokkanen@cgi.com>
2017-06-10 18:15:18 +0300

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/<template>")
+def hello(template):
+    return render_template(template + ".html", a = 5, text=1000)
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0")
